0e9af211d986aadc7aa7af83935acaa576edc9e8
rafalad <rafal-adamczyk@outlook.com>
2024-10-25 20:47:32 +0200

    Added html components

--- index.php
+++ index.php
@@ -9,3 +9,16 @@ catch (PODEXception $e) {
     exit();
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sklep instalacyjny</title>
+
+    <head>
+        <body>
+            <h1>Sklep instalacyjny</h1>
+</body>
+</html>
